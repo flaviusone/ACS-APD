207cafa1c342149f05ad22978218a7d25710159e
Flavius Tirnacop <flavius.tirnacop@cti.pub.ro>
2014-10-24 01:32:24 +0300

    [Tema-1] Adaugat functia mutate. Adaugat implementare seriala

--- Tema_1/tema1.c
+++ Tema_1/tema1.c
@@ -13,7 +13,59 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include "utils.h"
+
+#define GOL 0
+#define INDIVID 1
 /*-----  End of Includes & Defines  ------*/
+
+/**
+*
+* Functia mutate numara vecinii si evalueaza urmatoarea stare
+* Evident citeste din o matrice si pune rezultatu in alta
+*
+**/
+unsigned int mutate(unsigned short** matA, unsigned short** matB,
+	unsigned int i, unsigned int j){
+	int vecini = 0;
+	int k, l;
+	unsigned short element;
+	/* Count vecini */
+	for(k = -1 ; k < 2 ; ++k)
+		for(l = -1 ; l < 2 ; ++l)
+			vecini += matA[i+k][j+l];
+
+	element = matA[i][j];
+
+	if (element == GOL)
+	{
+		if (vecini == 3)
+		{
+			matB[i][j] = INDIVID;
+			return INDIVID;
+		}
+		else
+		{
+			matB[i][j] = GOL;
+			return GOL;
+		}
+	}
+	else
+	{
+		if (vecini == 2 || vecini == 3)
+		{
+			matB[i][j] = INDIVID;
+			return INDIVID;
+		}
+		else
+		{
+			matB[i][j] = GOL;
+			return GOL;
+		}
+	}
+
+
+	return -1;
+}
 
 int main(int argc, char **argv){
 
@@ -24,10 +76,12 @@ int main(int argc, char **argv){
 				 W, H,				/* Lungime si latime harta simulata */
 					N,				/* Number of steps */
 			 threadNo,				/* Number of threads */
-				 i, j;				/* Aux vars */
+				 i, j, res, 		/* Aux vars */
+				 flag_w, flag_h;	/* Aux vars */
 
 	unsigned short** matA;		/* Matrice de lucru veche*/
 	unsigned short** matB;		/* Matrice de lucru noua*/
+	unsigned short** auxmat;	/* Aux mat */
 	/*-----  End of Variabile declarate  ------*/
 
 	/*==============================================
@@ -43,8 +97,8 @@ int main(int argc, char **argv){
 	/* Deschide fisier citire */
 	f = fopen(argv[3], "r");
 	if (f == NULL) {
-    	perror("open");
-    	exit(EXIT_FAILURE);
+		perror("open");
+		exit(EXIT_FAILURE);
 	}
 
 	/* Citeste modul T sau P */
@@ -56,24 +110,24 @@ int main(int argc, char **argv){
 	fscanf(f, "%u", &W);
 	fscanf(f, "%u", &H);
 
-	/* Alocam matrice initiale */
-	matA = (unsigned short **)calloc(H, sizeof(unsigned short *));
-	matB = (unsigned short **)calloc(H, sizeof(unsigned short *));
-	for (i = 0; i < H; i++) {
-		matA[i] = (unsigned short *)calloc(W, sizeof(unsigned short));
-		matB[i] = (unsigned short *)calloc(W, sizeof(unsigned short));
+	/* Alocam matrice initiale bordate cu 0*/
+	matA = (unsigned short **)calloc(H+2, sizeof(unsigned short *));
+	matB = (unsigned short **)calloc(H+2, sizeof(unsigned short *));
+	for (i = 0; i < H+2; i++) {
+		matA[i] = (unsigned short *)calloc(W+2, sizeof(unsigned short));
+		matB[i] = (unsigned short *)calloc(W+2, sizeof(unsigned short));
 	}
 	/* Citire matrice din fisier */
-	for(i = 0 ; i < H_harta ; i++){
-		for(j = 0 ; j < W_harta ; j++){
+	for(i = 1 ; i < H_harta+1 ; ++i){
+		for(j = 1 ; j < W_harta+1 ; ++j){
 			fscanf(f,"%hu",&matA[i][j]);
 		}
 	}
 	/* Deschide fisier pentru scriere */
 	f = fopen(argv[4], "w+");
 	if (f == NULL) {
-    	perror("open");
-    	exit(EXIT_FAILURE);
+		perror("open");
+		exit(EXIT_FAILURE);
 	}
 	/*-----  End of Citiri & initializari  ------*/
 
@@ -83,18 +137,49 @@ int main(int argc, char **argv){
 	=========================================*/
 
 	while(N > 0){
-		for(i = 0 ; i < H_harta+1 ; ++i){
-			for(j = 0 ; j < W_harta+1 ; ++j){
-				// Call Mutate
-				// Daca este pe margine si a devenit alive W_harta/H_harta++
-			}
+		for(i = 1 ; i < H_harta ; ++i)
+			for(j = 1 ; j < W_harta ; ++j)
+				mutate(matA, matB, i, j);
+
+
+		/* Verificam marginile pentru a vedea daca
+		 * trebuie extinsa suprafata actuala de verificare */
+		flag_h = flag_w = 0;
+		for(i = 1 ; i < H_harta ; ++i){
+			if(W_harta == W)
+				res = mutate(matA, matB, i, W_harta);
+			else
+				res = mutate(matA, matB, i, W_harta + 1);
+			if (res == INDIVID)
+				flag_w = 1;
 		}
+
+		for(j = 1 ; j < W_harta ; ++j){
+			if(H_harta == H)
+				res = mutate(matA, matB, H_harta, j);
+			else
+				res = mutate(matA, matB, H_harta + 1, j);
+			if (res == INDIVID)
+				flag_h = 1;
+		}
+
+		if (flag_h && H_harta!=H) ++H_harta;
+		if (flag_w && W_harta!=W) ++W_harta;
+
+		/* Update sides if Toroid */
+		// if(mode == "T"){
+		// }
+
+		/* Swap the 2 matrices */
+		auxmat = matA;
+		matA = matB;
+		matB = auxmat;
 
 
 		N--;
 	}
 
-	print_fisier(f, matA, W, H);
+	print_fisier(f, matA, mode, W_harta, H_harta, W, H);
 	/*-----  End of Secitune Algoritm  ------*/
 
 
--- Tema_1/utils.h
+++ Tema_1/utils.h
@@ -1,17 +1,21 @@
-void print_consola(unsigned short** mat, unsigned int W, unsigned int H){
+void print_consola(FILE* f, unsigned short** mat, char mode,
+		 	unsigned int W_h, unsigned int H_h, unsigned int W, unsigned int H){
 	int i,j;
-	for(i = 0 ; i < H ; i++){
-		for(j = 0 ; j < W ; j++){
+	printf("%c %u %u %u %u\n", mode, W_h, H_h, W, H);
+	for(i = 1 ; i < H ; i++){
+		for(j = 1 ; j < W ; j++){
 			printf("%hu ",mat[i][j]);
 		}
 		printf("\n");
 	}
 }
 
-void print_fisier(FILE* f, unsigned short** mat, unsigned int W, unsigned int H){
+void print_fisier(FILE* f, unsigned short** mat, char mode,
+		 	unsigned int W_h, unsigned int H_h, unsigned int W, unsigned int H){
 	int i,j;
-	for(i = 0 ; i < H ; i++){
-		for(j = 0 ; j < W ; j++){
+	fprintf(f,"%c %u %u %u %u\n", mode, W_h, H_h, W, H);
+	for(i = 1 ; i < H+1 ; i++){
+		for(j = 1 ; j < W+1 ; j++){
 			fprintf(f,"%hu ",mat[i][j]);
 		}
 		fprintf(f,"\n");
