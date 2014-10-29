7de526838c89d494e593ed099d363c9ceede1800
Flavius Tirnacop <flavius.tirnacop@cti.pub.ro>
2014-10-29 20:23:10 +0200

    [Tema-1] Cleanup cod. Scris readme. Done

--- Tema_1/tema1.c
+++ Tema_1/tema1.c
@@ -3,7 +3,7 @@
 * Author: Flavius Tirnacop
 * Grupa: 341C1
 * Fisier: serial.c
-* Descriere: Implementare seriala Conway's Game of Life
+* Descriere: Implementare paralela Conway's Game of Life
 *
 **/
 
@@ -16,93 +16,8 @@
 #include <omp.h>
 #include <limits.h>
 
-#define GOL 0
-#define INDIVID 1
+
 /*-----  End of Includes & Defines  ------*/
-
-/**
-*
-* Functia mutate numara vecinii si evalueaza urmatoarea stare
-* Evident citeste din o matrice si pune rezultatu in alta
-*
-**/
-static void update_sides(unsigned short** matA, unsigned short** matB,
-	unsigned int W_harta,unsigned int H_harta, unsigned int W,unsigned int H)
-{
-	register int i,j;
-
-	/* Top & Bottom */
-	#pragma omp parallel for private(j) shared(H, W, matA, matB)\
-	schedule(static)
-	for (j = 1; j < W_harta+1; ++j){
-		matB[0][j] = matB[H][j];
-		matB[H+1][j] = matB[1][j];
-	}
-
-	/* Sides */
-	#pragma omp parallel for private(i) shared(H, W, matA, matB)\
-	schedule(static)
-	for (i = 1; i < H_harta+1; ++i){
-		matB[i][0] = matB[i][W];
-		matB[i][W+1] = matB[i][1];
-	}
-
-	/* Corners */
-	#pragma omp parallel shared(matB, H, W)
-	{
-		matB[0][0] = matB[H][W];
-		matB[H+1][W+1] = matB[1][1];
-		matB[0][W+1] = matB[H][1];
-		matB[H+1][0] = matB[1][W];
-	}
-}
-
-static unsigned int mutate(unsigned short** matA, unsigned short** matB,
-	unsigned int i, unsigned int j){
-	unsigned short vecini = 0;
-	/* Count vecini */
-	vecini = vecini + \
-			matA[i-1][j-1] 	+ 	matA[i-1][j] 	+\
-			matA[i-1][j+1] 	+ 	matA[i][j+1] 	+\
-			matA[i][j-1] 	+ 	matA[i+1][j-1] 	+\
-			matA[i+1][j] 	+	matA[i+1][j+1];
-
-	// if( vecini < 4){
-	// 	vecini += matA[i+1][j-1] + matA[i+1][j]	+ matA[i+1][j+1];
-	// }
-
-	if (matA[i][j] == GOL)
-	{
-		if (vecini == 3)
-		{
-			matB[i][j] = INDIVID;
-			return INDIVID;
-		}
-		else
-		{
-			matB[i][j] = GOL;
-			return GOL;
-		}
-	}
-	else
-	{
-		if (vecini == 2 || vecini == 3)
-		{
-			matB[i][j] = INDIVID;
-			return INDIVID;
-		}
-		else
-		{
-			matB[i][j] = GOL;
-			return GOL;
-		}
-	}
-
-
-	return -1;
-}
-
-
 
 int main(int argc, char **argv){
 
@@ -112,14 +27,14 @@ int main(int argc, char **argv){
 	unsigned int W_harta, H_harta,	/* Lungime si latime harta */
 				 W, H,				/* Lungime si latime harta simulata */
 					N;				/* Number of steps */
-	unsigned int 					/* Number of threads */
+	register unsigned int			/* Aux vars */
 				 i, j,
-				 k,l;				/* Aux vars */
-
+				 varaux_h,varaux_w,
+				 k,l;
 	unsigned short** matA;		/* Matrice de lucru veche*/
 	unsigned short** matB;		/* Matrice de lucru noua*/
 	unsigned short** auxmat;	/* Aux mat */
-	char* buff = malloc(sizeof(char));
+	char* buff = malloc(100*sizeof(char)); /* Aux var */
 
 	/*-----  End of Variabile declarate  ------*/
 
@@ -153,16 +68,9 @@ int main(int argc, char **argv){
 	if(H_harta > H) H_harta = H;
 
 	/* Alocam matrice initiale bordate cu 0*/
-	// matA = allocMat(W+2, H+2);
-	// matB = allocMat(W+2, H+2);
-	matA = (unsigned short **)calloc(H+2, sizeof(unsigned short *));
-	matB = (unsigned short **)calloc(H+2, sizeof(unsigned short *));
+	matA = allocMat(W+2, H+2);
+	matB = allocMat(W+2, H+2);
 
-	#pragma omp parallel for private(i)
-	for (i = 0; i < H+2; i++) {
-		matA[i] = (unsigned short *)calloc(W+2, sizeof(unsigned short));
-		matB[i] = (unsigned short *)calloc(W+2, sizeof(unsigned short));
-	}
 	/* Citire matrice din fisier */
 	for(i = 1 ; i < H_harta+1 ; ++i){
 		for(j = 1 ; j < W_harta+1 ; ++j){
@@ -170,16 +78,6 @@ int main(int argc, char **argv){
 		}
 		fgets(buff, INT_MAX, f);
 	}
-	fclose(f);
-
-	/* Deschide fisier pentru scriere */
-	f = fopen("stuff.vmr", "a");
-	if (f == NULL) {
-		perror("open");
-		exit(EXIT_FAILURE);
-	}
-	fprintf(f,"%c %u %u %u %u\n", mode, W_harta, H_harta, W, H);
-
 	fclose(f);
 
 	/* Deschide fisier pentru scriere */
@@ -197,22 +95,15 @@ int main(int argc, char **argv){
 
 	while(N > 0){
 
-		// print_consola(matA, mode, W_harta, H_harta, W, H);
-		// getchar();
-
 		/* Update sides if Toroid */
 		if(mode == 'T'){
-			update_sides(matA, matA, W, H, W, H);
+			update_toroid(matA, matA, W, H);
 		}
-
-		// print_consola(matA, mode, W_harta, H_harta, W, H);
-		// getchar();
-
 
 		/* Varianta de baza */
 		// #pragma omp parallel for collapse(2)\
 		// shared(H,W,matA,matB)\
-		// private(i,j) schedule(static)
+		// private(i,j) schedule(auto)
 		// for(i = 1 ; i < H + 1 ; ++i)
 		// 	for(j = 1 ; j < W + 1 ; ++j){
 		// 		// printf("Thread %d cu %d %d\n", omp_get_thread_num(), i, j);
@@ -220,61 +111,44 @@ int main(int argc, char **argv){
 		// 	}
 
 		/* Varianta de test */
-		int varaux = 2 * atoi(argv[1]);
-		#pragma omp parallel for collapse(2) private(i,j,k,l) schedule(static)
-		for( k = 0 ; k < varaux ; k++){
-			for( l = 0 ; l < varaux ; l++){
-				for(i = 1 + k *  H / varaux; i < (k+1) *  H / varaux + 1 ; ++i)
-					for(j = 1 + l * W / varaux ; j < (l+1) * W / varaux + 1 ; ++j){
-						mutate(matA, matB, i, j);
-						// printf("Thread %d cu %d %d\n", omp_get_thread_num(), i, j);
-					}
-			}
-		}
+		varaux_h = H/atoi(argv[1]);
+		varaux_w = W/atoi(argv[1]);
 
-		// print_consola(matA, mode, W_harta, H_harta, W, H);
-		// getchar();
-
-
-
-		// print_consola(matA, mode, W_harta, H_harta, W, H);
-		// getchar();
+		/* Parcurgem matricea pe blocuri de W/n_threads H/n_threads
+		 * pentru a reduce din cache miss-uri */
+		#pragma omp parallel for private(i,j,k,l) schedule(static)
+		for( k = 0 ; k < varaux_h ; ++k)
+			for( l = 0 ; l < varaux_w ; ++l)
+				for(i = 1 + k *  H / varaux_h; i < (k+1) *  H / varaux_h + 1 ; ++i)
+					for(j = 1 + l * W / varaux_w ; j < (l+1) * W / varaux_w + 1 ; ++j)
+						mutate(matA, matB, i, j, W+2, H+2);
 
 		/* Swap the 2 matrices */
 		auxmat = matA;
 		matA = matB;
 		matB = auxmat;
 
-		// #pragma omp parallel for schedule(static) private(i)
-		// for (i = 0; i < H+2; i++) {
-		// 	memset(matB[i], 0 , (W+2) * sizeof(unsigned short));
-		// }
-
 		N--;
 
 	}
-	W_harta = 1;
-	H_harta = 1;
 
 	/* Recalculam W_harta si H_harta */
 	#pragma omp parallel for private(i,j) shared(matA,W_harta,H_harta) schedule(static)
 	for (i = 1; i < H+1; ++i)
-	{
-		for (j = W_harta; j < W+1; ++j)
-		{
-
-			if(matA[i][j]){
+		for (j = 1; j < W+1; ++j)
+			if(matA[i][j])
+			{
 				if(i>H_harta)H_harta = i;
 				if(j>W_harta)W_harta = j;
 			}
-		}
-	}
 
-	print_fisier(f, matA, mode, W_harta, H_harta, W, H);
 	/*-----  End of Secitune Algoritm  ------*/
 
+	/* Scriem output in fisier */
+	print_fisier(f, matA, mode, W_harta, H_harta, W, H);
 
 	/* Inchidem fisierul de input */
 	fclose(f);
+
 	return 0;
 }
--- Tema_1/utils.h
+++ Tema_1/utils.h
@@ -1,3 +1,108 @@
+/**
+*
+* Author: Flavius Tirnacop
+* Grupa: 341C1
+* Fisier: utils.h
+* Descriere: Include functii ajutatoare pentru algoritmul principal
+*
+**/
+
+#define GOL 0
+#define INDIVID 1
+
+
+/**
+*
+* Rezolva dependentele marginilor in cazul modului toroid
+*
+**/
+static void update_toroid(unsigned short** matA, unsigned short** matB,
+						unsigned int W, unsigned int H)
+{
+	register int i,j;
+
+	/* Top & Bottom */
+	#pragma omp parallel for private(j) shared(H, W, matA, matB)\
+	schedule(static)
+	for (j = 1; j < W+1; ++j){
+		matB[0][j] = matB[H][j];
+		matB[H+1][j] = matB[1][j];
+	}
+
+	/* Sides */
+	#pragma omp parallel for private(i) shared(H, W, matA, matB)\
+	schedule(static)
+	for (i = 1; i < H+1; ++i){
+		matB[i][0] = matB[i][W];
+		matB[i][W+1] = matB[i][1];
+	}
+
+	/* Corners */
+	matB[0][0] = matB[H][W];
+	matB[H+1][W+1] = matB[1][1];
+	matB[0][W+1] = matB[H][1];
+	matB[H+1][0] = matB[1][W];
+
+}
+
+/**
+*
+* Functia mutate numara vecinii si evalueaza urmatoarea stare
+* Evident citeste din o matrice si pune rezultatu in alta
+*
+**/
+static unsigned int mutate(unsigned short** matA, unsigned short** matB,
+	unsigned int i, unsigned int j, unsigned int W, unsigned int H){
+
+	register unsigned short vecini = 0;
+
+	/* Count vecini */
+	unsigned short* pij = &matA[i][j];
+	vecini += *(pij-W-1) + *(pij-W) + *(pij-W+1) ;
+	vecini += *(pij-1) + *(pij+1);
+
+
+	/* Incercam sa evitam op suplimentare daca deja s-au gasit destui vecini */
+	if( vecini < 4){
+		vecini += *(pij+W-1) + *(pij+W) + *(pij+W+1) ;
+	}
+
+	if (matA[i][j] == GOL)
+	{
+		if (vecini == 3)
+		{
+			matB[i][j] = INDIVID;
+			return INDIVID;
+		}
+		else
+		{
+			matB[i][j] = GOL;
+			return GOL;
+		}
+	}
+	else
+	{
+		if (vecini == 2 || vecini == 3)
+		{
+			matB[i][j] = INDIVID;
+			return INDIVID;
+		}
+		else
+		{
+			matB[i][j] = GOL;
+			return GOL;
+		}
+	}
+
+
+	return -1;
+}
+
+/**
+*
+* Afiseaza matricea la consola.For debug purposes only.
+*
+**/
 void print_consola(unsigned short** mat, char mode,
 		 	unsigned int W_h, unsigned int H_h, unsigned int W, unsigned int H){
 	int i,j;
@@ -10,6 +115,11 @@ void print_consola(unsigned short** mat, char mode,
 	}
 }
 
+/**
+*
+* Scrie matricea in fisier
+*
+**/
 void print_fisier(FILE* f, unsigned short** mat, char mode,
 		 	unsigned int W_h, unsigned int H_h, unsigned int W, unsigned int H){
 	int i,j;
@@ -22,7 +132,11 @@ void print_fisier(FILE* f, unsigned short** mat, char mode,
 	}
 }
 
-// alocare in zona continua de memorie
+/**
+*
+* Functie de alocare in zona continua de memorie
+*
+**/
 unsigned short **allocMat(int w, int h)
 {
     unsigned short **mat;
@@ -35,3 +149,4 @@ unsigned short **allocMat(int w, int h)
         mat[i] = buf + i * w;
     return mat;
 }
+
